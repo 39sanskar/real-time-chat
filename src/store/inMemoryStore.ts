e13b1e4bce9e3995ab585f3f04831e9abb9dd9ef
39sanskar <mishrasanskar496@gmail.com>
2025-12-28 02:00:48 +0530

    added message handlers

---
+++ src/store/inMemoryStore.ts
@@ -0,0 +1,65 @@
+import { Store, type Chat, type UserId } from "./Store.js";
+let globalChatId = 0;
+export interface Room {
+  roomId: string;
+  chats: Chat[];
+}
+export class InMemoryStore implements Store {
+  private store: Map<string, Room>;
+  constructor() {
+    this.store = new Map<string, Room>()
+  }
+  initRoom(roomId: string) {
+    this.store.set(roomId, {
+      roomId,
+      chats: []
+    });
+  }
+
+  // if i need last 50 chats => limit = 50, offset = 0
+  // if i need the next 50 chats => limit = 50, offset = 50
+  getChats(roomId: string, limit: number, offset: number) {
+    const room = this.store.get(roomId);
+    if (!room) {
+      return []
+    }
+    return room.chats.reverse().slice(0, offset).slice(-1 * limit);
+  }
+
+  addChat(userId: UserId, name: string, roomId: string, message: string) {
+    const room = this.store.get(roomId);
+    if (!room) {
+      return
+    }
+    const chat = {
+      id: (globalChatId++).toString(),
+      userId,
+      name,
+      message,
+      upvotes: []
+    }
+    room.chats.push(chat)
+    return chat;
+  }
+
+  upvote(userId: UserId, roomId: string, chatId: string) {
+    const room = this.store.get(roomId);
+    if (!room) {
+      return
+    }
+    // Todo: Make this faster
+    const chat = room.chats.find(({ id }) => id == chatId);
+
+    if (chat) {
+      if (chat.upvotes.find(x => x === userId)) {
+        return chat;
+      }
+      chat.upvotes.push(userId);
+    }
+    return chat;
+  }
+}
+
+
+
+
